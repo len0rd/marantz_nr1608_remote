c206f5e390cc8e7062a8330c99018bcfd372e741
len0rd <tylerm15@gmail.com>
2019-09-24 19:29:07 -0600

    progress. ready to add my own submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
+
+// marantx uses a mix of RC5 and RC5x
+#define SEND_RC5
+#define SEND_RC5X
+
 #include <IRremote.h>
+
+#define LED 13
 
 IRsend irsend;
 
-static unsigned int power_toggle[] = {0x0000, 0x0073, 0x0000, 0x0018, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0040, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0040, 0x0020, 0x0020, 0x0040, 0x0020, 0x0020, 0x0C5B, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0040, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0020, 0x0040, 0x0020, 0x0020, 0x0040, 0x0020, 0x0020, 0x0498};
+static unsigned int power_toggle[] = { 900,900,  900,900,   900,900,  1750,900, 900,900,  900,900,  900,900,  900,900,  900,1750,  900,900,  1750,900, 900 };
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   int khz = 38; // 38kHz carrier frequency for the NEC protocol
+  static int led_state = HIGH;
   
-  irsend.sendRaw(power_toggle, sizeof(power_toggle) / sizeof(power_toggle[0]), khz); //Note the approach used to automatically calculate the size of the array.
+  digitalWrite(LED_BUILTIN, led_state);
+  digitalWrite(LED, HIGH);
+  // irsend.sendRaw(power_toggle, sizeof(power_toggle) / sizeof(power_toggle[0]), khz); //Note the approach used to automatically calculate the size of the array.
+  irsend.sendRC5x();
+  digitalWrite(LED, LOW);
 
-  delay(10000); // 10s repeat
+  led_state = led_state == HIGH ? LOW : HIGH;
+  delay(5000); // 10s repeat
 }
+
+
+
