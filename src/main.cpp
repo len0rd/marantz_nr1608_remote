bdb2fa989810d28f256aa96af10c41d8ddf8c09d
len0rd <tylerm15@gmail.com>
2019-09-24 21:59:07 -0600

    power cycling working. cant get normal RC5 to work.

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,12 @@
 
 #define LED 13
 
+#define ON  1
+#define OFF 2
+
 IRsend irsend;
 
-static unsigned int power_toggle[] = { 900,900,  900,900,   900,900,  1750,900, 900,900,  900,900,  900,900,  900,900,  900,1750,  900,900,  1750,900, 900 };
+// static unsigned int power_toggle[] = { 900,900,  900,900,   900,900,  1750,900, 900,900,  900,900,  900,900,  900,900,  900,1750,  900,900,  1750,900, 900 };
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -19,16 +22,24 @@ void setup() {
 
 void loop() {
   int khz = 38; // 38kHz carrier frequency for the NEC protocol
-  static int led_state = HIGH;
+  static int avr_state = OFF;
   
-  digitalWrite(LED_BUILTIN, led_state);
   digitalWrite(LED, HIGH);
   // irsend.sendRaw(power_toggle, sizeof(power_toggle) / sizeof(power_toggle[0]), khz); //Note the approach used to automatically calculate the size of the array.
-  irsend.sendRC5x();
+  if (avr_state == OFF) {
+    irsend.sendRC5x(0, 16, 12, 1); // power on
+    delay(100);
+    irsend.sendRC5x(1, 16, 12, 1); // power on
+  } 
+  else {
+    irsend.sendRC5x(0, 16, 12, 2); // power off
+    delay(100);
+    irsend.sendRC5x(1, 16, 12, 2); // power off
+  }
   digitalWrite(LED, LOW);
 
-  led_state = led_state == HIGH ? LOW : HIGH;
-  delay(5000); // 10s repeat
+  avr_state = avr_state == ON ? OFF : ON;
+  delay(5000);
 }
 
 
